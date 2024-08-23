ec453f4b983a618f4c6fb875d8bc08ac52e81bc9
Alejandro Páez Espinosa <apaez@dedgesecurity.com>
2024-08-23 13:58:38 +0200

    Revert "Update MockERC20.sol to test ext-tools"
    This reverts commit 4638a562abdf182d46d51a439ebbf2be297783f5.

--- taurus-contracts/contracts/Mocks/MockERC20.sol
+++ taurus-contracts/contracts/Mocks/MockERC20.sol
@@ -6,6 +6,10 @@ import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract MockERC20 is ERC20 {
     uint256 public constant PRECISION = 1e18;
 
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        _mint(tx.origin, PRECISION * PRECISION);
+    }
+
     function mint(address to, uint256 amount) external {
         _mint(to, amount);
     }
